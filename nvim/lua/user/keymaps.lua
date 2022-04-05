1ede33978cb8ef398956365d4f1a493d8a48fdd6
Joel Farthing <joel.f.farthing@gmail.com>
2022-04-05 08:43:21 -0600

    add keymaps for cycling buffers

--- nvim/lua/user/keymaps.lua
+++ nvim/lua/user/keymaps.lua
@@ -43,3 +43,7 @@ keymap("n", "<leader>e", ":NvimTreeToggle<cr>", opts)
 keymap("n", "<leader>f", "<cmd>lua require'telescope.builtin'.find_files(require('telescope.themes').get_dropdown({ previewer = false }))<cr>", opts)
 keymap("n", "<c-t>", "<cmd>Telescope live_grep<cr>", opts)
 
+-- Cycle Buffers
+keymap("n", "<S-l>", ":bnext<CR>", opts)
+keymap("n", "<S-h>", ":bprevious<CR>", opts)
+
